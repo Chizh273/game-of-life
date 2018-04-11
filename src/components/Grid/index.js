fd4fa0f2751bdcbbe33b32801ede1d91cd5b1ad0
chizh273 <chizh273@gmail.com>
2018-04-11 08:49:00 +0300

    Added Grid component

---
+++ src/components/Grid/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Grid(props) {
+    return 'grid'
+}
+
+Grid.propTypes = {
+    size: PropTypes.number.isRequired
+}
